90883f027dd4c369d9fd93a85cc9726a5dd965e7
chenyao <haiiey888@qq.com>
2020-07-16 16:24:45 +0800

    app clip

---
+++ Qi_SwiftUI_Demo/Qi_SwiftUI Clip/ContentView.swift
@@ -0,0 +1,102 @@
+//
+//  ContentView.swift
+//  Qi_SwiftUI Clip
+//
+//  Created by chenyao on 2020/7/16.
+//  Copyright © 2020 QiShare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isLeftNav = false
+    @State var isRightNav = false
+    
+    init() {
+        //修改导航栏文字颜色
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        UINavigationBar.appearance().tintColor = .systemBlue
+    }
+    
+    var body: some View {
+        #if !APPCLIP
+        TabView {
+            // Tab1:
+            NavigationView {
+                List(Symbols, id:\.self) {
+                    ListRow(symbol: $0)
+                }
+                .navigationBarTitle(Text("SF Symbols Clip"))
+                .navigationBarItems(leading: leftNavButton, trailing: rightNavButton)
+            }.tabItem {
+                Image.init(systemName: "star.fill")
+                Text("Tab1").font(.subheadline)
+            }
+            
+            // Tab2:
+            NavigationView {
+                Text("This is the second tab.")
+            }.tabItem {
+                Image.init(systemName: "star.fill")
+                Text("Tab2").font(.subheadline)
+            }
+        }
+        #else
+        NavigationView {
+            List(Symbols, id:\.self) {
+                ListRow(symbol: $0)
+            }
+            .navigationBarTitle(Text("SF Symbols Clip"))
+            .navigationBarItems(leading: leftNavButton, trailing: rightNavButton)
+        }
+        #endif
+    }
+    
+    var leftNavButton: some View {
+        Button(action: { self.isLeftNav.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("Left"))
+                .padding()
+        }
+        .sheet(isPresented: $isLeftNav) {
+            VStack {
+                Text("Hello, we are QiShare!").foregroundColor(.blue).font(.system(size: 32.0))
+
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Text("an iOS Team. ").fontWeight(.black).foregroundColor(.purple)
+                    Spacer()
+                    Text("We are learning SwiftUI.").foregroundColor(.blue)
+                    Spacer()
+                }
+            }
+        }
+    }
+    
+    var rightNavButton: some View {
+        Button(action: { self.isRightNav.toggle() }) {
+            Image(systemName: "paperplane.fill")
+                .imageScale(.large)
+                .accessibility(label: Text("Right"))
+                .padding()
+        }
+        .sheet(isPresented: $isRightNav, onDismiss: {
+            print("dissmiss RrightNav")
+        }) {
+            ZStack {
+                Text("This is the Right Navi Button.")
+            }
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qi_SwiftUI_Demo/Qi_SwiftUI Clip/Qi_SwiftUI_ClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  Qi_SwiftUI_ClipApp.swift
+//  Qi_SwiftUI Clip
+//
+//  Created by chenyao on 2020/7/16.
+//  Copyright © 2020 QiShare. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Qi_SwiftUI_ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
